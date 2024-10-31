3be7c29a451151d671b53df95f64d3e92519ebf0
Hung Dung <hungdung.nnl@gmail.com>
2024-10-31 14:48:57 +0700

    Create main.java

---
+++ BTH2/main.java
@@ -0,0 +1,20 @@
+ public static void main(String[] args) {
+        Diem d1 = new Diem(9, 10);
+        Diem d2 = new Diem(1, 10);
+        d1.hienThiDiem();
+        d2.hienThiDiem();
+        System.out.printf("\nKhoanh cach giua 2 diem la: %.1f\n", d1.tinhKhoangCach(d2));
+
+        DoanThang doan = new DoanThang(d1, d2);
+        doan.hienThiDoan();
+
+        DoanThang doan1 = new DoanThang(new Diem(1, 5), new Diem(8, 5));
+        DoanThang doan2 = new DoanThang(new Diem(2, 3), new Diem(4, 3));
+        System.out.printf("\nDo dai doan = %.1f\n", doan1.tinhDoDai());
+        Diem trungDiem = doan1.timTrungDiem();
+        System.out.print("Trung diem cua doan la: ");
+        trungDiem.hienThiDiem();
+        System.out.println();
+        System.out.println(doan1.isSongSong(doan2));
+    }
+
